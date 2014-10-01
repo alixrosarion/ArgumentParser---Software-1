a4676f6729651374c2b9bcba453035e34929cf3a
Sari <ssabouh@jsu.edu>
2014-09-30 19:30:32 -0500

    I actually made the tests pass this time hahaha

--- src/test/java/edu/jsu/mcis/ArguementParserTest.java
+++ src/test/java/edu/jsu/mcis/ArguementParserTest.java
@@ -15,7 +15,7 @@ public class ArguementParserTest {
 	
 	@Test
 	public void testCommandLine () {
-		ArguementParser tester = new ArguementParser("7", "5","8");
+		ArguementParser tester = new ArguementParser("7", "5","2");
 		assertEquals("7", tester.getArg1());
 		assertEquals("5", tester.getArg2());
 		assertEquals("2", tester.getArg3());
