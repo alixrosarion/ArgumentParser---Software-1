9ff82778c692f7befd0eb128c21d7acfc55dd890
Alix <alixrosarion@gmail.com>
2014-10-01 20:12:34 -0500

    fresh start

--- src/test/java/edu/jsu/mcis/ArgumentParserTest.java
+++ src/test/java/edu/jsu/mcis/ArgumentParserTest.java
@@ -3,7 +3,6 @@ package edu.jsu.mcis;
 import org.junit.*;
 import static org.junit.Assert.*;
 
-<<<<<<< HEAD
 public class ArgumentParserTest {
 
 	@Test
@@ -32,23 +31,3 @@ public class ArgumentParserTest {
 		assertEquals("7", tester.getArgumentValue("length"));
 	}
 }
-=======
-public class ArguemntParserTest {
-
-	@Test
-	public void testEmptyStrings () {
-		ArgumentParser tester = new ArgumentParser("", "", "");
-		assertEquals("", tester.getArg1());
-		assertEquals("", tester.getArg2());
-		assertEquals("", tester.getArg3());
-	}
-	
-	@Test
-	public void testCommandLine () {
-		ArgumentParser tester = new ArgumentParser("7", "5","2");
-		assertEquals("7", tester.getArg1());
-		assertEquals("5", tester.getArg2());
-		assertEquals("2", tester.getArg3());
-	}
-}
->>>>>>> 5b4b847ed55764bfa5a357a99b1a400b84276090
