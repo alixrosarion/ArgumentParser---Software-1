1d2a61bf3d3f434e0629c4d4f8ef7426216bd234
SariSabouh <ssabouh@jsu.edu>
2014-11-08 19:21:29 -0600

    renamed it forgot hahhaa

--- VolCalcLoadXML.java
+++ VolCalcLoadXML.java
@@ -2,31 +2,23 @@ import edu.jsu.mcis.*;
 
 import java.util.*;
 @SuppressWarnings("unchecked")
-public class VolCalcLoadXML
+public class VolCalcXML
 {
 	public static void main(String [] args) 
 	{
-		ArgumentParser parser = new ArgumentParser();
-		String read = "";
-		String filename = args[0];
-		parser.addArgument("length", CommandLineArgument.Type.Float, "the length of the box");
-		parser.addArgument("width", CommandLineArgument.Type.Float, "the width of the box");
-		parser.addArgument("height", CommandLineArgument.Type.Float, "the height of the box");
+		ArgumentParser parser = XMLParser.createArgumentParser("test.xml");
 		parser.addProgram("VolCalc","Calculates the volume of an object");
-		parser.addOptArg("-h", 0);
-		
+		String read = "";
 		for(String arg:args){
-			if(!arg.contains(".xml")) read += arg + " ";
-			
+			read += arg + " ";
 		}try{
 			parser.parse(read);
-			parser.writeToXMLFile(filename);
 			try{
             float length = parser.getArgumentValue("length");
 			float height = parser.getArgumentValue("height");
 			float width = parser.getArgumentValue("width");
 			System.out.println("Volume is " + length * width * height);
-			} catch(Exception e){}
+			} catch(NullPointerException e){}
 			System.exit(1);
 		}
 		catch(Exception e) { e.printStackTrace(); }
---
+++ VolCalcSaveXML.java
@@ -0,0 +1,37 @@
+import edu.jsu.mcis.*;
+
+import java.util.*;
+@SuppressWarnings("unchecked")
+public class VolCalcLoadXML
+{
+	public static void main(String [] args) 
+	{
+		ArgumentParser parser = new ArgumentParser();
+		String read = "";
+		String filename = args[0];
+		parser.addArgument("length", CommandLineArgument.Type.Float, "the length of the box");
+		parser.addArgument("width", CommandLineArgument.Type.Float, "the width of the box");
+		parser.addArgument("height", CommandLineArgument.Type.Float, "the height of the box");
+		parser.addProgram("VolCalc","Calculates the volume of an object");
+		parser.addOptArg("-h", 0);
+		
+		for(String arg:args){
+			if(!arg.contains(".xml")) read += arg + " ";
+			
+		}try{
+			parser.parse(read);
+			parser.writeToXMLFile(filename);
+			try{
+            float length = parser.getArgumentValue("length");
+			float height = parser.getArgumentValue("height");
+			float width = parser.getArgumentValue("width");
+			System.out.println("Volume is " + length * width * height);
+			} catch(Exception e){}
+			System.exit(1);
+		}
+		catch(Exception e) { e.printStackTrace(); }
+		
+		
+	}
+}
+
